60db8b952365c1d17677a03cc26cbe006d662b86
Sudheer Satyanarayana <sudheer.zzz@sudheer.net>
2017-10-02 10:30:46 +0530

    Fixes

--- templates/unicorn.conf.rb
+++ templates/unicorn.conf.rb
@@ -2,13 +2,14 @@ worker_processes {{ unicorn_worker_processes }}
 
 working_directory "{{ redmine_home }}" # available in 0.94.0+
 
-listen "/var/run/redmine/redmine.unicorn.sock", :backlog => 64
+listen 8080, :tcp_nopush => true
+#listen "/var/run/redmine/redmine.unicorn.sock", :backlog => 64
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/var/run/redmine/unicorn.pid"
+#pid "/var/run/redmine/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
