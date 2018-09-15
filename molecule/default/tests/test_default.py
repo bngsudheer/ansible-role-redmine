fcae81fe71766cb70395ed4d88a91cb5800e3c3f
Sudheer Satyanarayana <sudheer.zzz@sudheer.net>
2018-09-16 02:51:32 +0530

    Two blank lines between tests. Make another linter happy.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,40 +13,49 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_nginx_is_installed(host):
     nginx = host.package("nginx")
     assert nginx.is_installed
+
 
 def test_nginx_running_and_enabled(host):
     nginx = host.service("nginx")
     assert nginx.is_running
     assert nginx.is_enabled
 
+
 def test_nginx_running_and_enabled(host):
     firewalld = host.service("firewalld")
     assert firewalld.is_running
     assert firewalld.is_enabled
+
 
 def test_unicorn_is_running(host):
     master = host.process.filter(user='redmine', comm='unicorn_rails')
     for p in master:
         assert 'unicorn' in p
 
+
 def unicorn_is_listening(host):
     assert host.socket("tcp://5777").is_listening == True
+
 
 def test_redmine_running_and_enabled(host):
     redmine = host.service("redmine")
     assert redmine.is_running
     assert redmine.is_enabled
 
+
 def test_ruby_packages_installed(host):
     for package_name in ['ImageMagick', 'ruby-devel']:
         package = host.package(package_name)
         assert package.is_installed
 
+
 def test_redmine_user(host):
      assert 'redmine' == host.user("redmine").name
+
 
 def test_redmine_db_configuration_file(host):
      # assert host.file("/home/redmine/redmine/config/configuration.yml").exists
